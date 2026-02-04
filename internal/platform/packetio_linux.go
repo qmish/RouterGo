19506b268db3b96ebf69784782760d568de03c80
qmishnik <maxim@quantum-mishnik.ru>
2026-02-04 20:24:18 +0300

    Добавить linux PacketIO заглушку
    NewPacketIO для Linux возвращает объект с Read/Write, которые возвращают ErrNotSupported. Добавлен тест.

--- internal/platform/packetio_linux.go
+++ internal/platform/packetio_linux.go
@@ -2,8 +2,26 @@
 
 package platform
 
-import "router-go/pkg/network"
+import (
+	"context"
+
+	"router-go/pkg/network"
+)
+
+type linuxPacketIO struct{}
 
 func NewPacketIO(opts Options) (network.PacketIO, error) {
-	return nil, ErrNotSupported
+	return &linuxPacketIO{}, nil
+}
+
+func (l *linuxPacketIO) ReadPacket(ctx context.Context) (network.Packet, error) {
+	return network.Packet{}, ErrNotSupported
+}
+
+func (l *linuxPacketIO) WritePacket(ctx context.Context, pkt network.Packet) error {
+	return ErrNotSupported
+}
+
+func (l *linuxPacketIO) Close() error {
+	return nil
 }
